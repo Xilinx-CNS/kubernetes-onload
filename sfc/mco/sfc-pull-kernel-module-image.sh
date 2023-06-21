c32f7b56eb97a1ac8e7ba301c0ab41596c5baccb
Thomas Crawley <thomas.crawley@xilinx.com>
2023-06-21 10:57:31 +0100

    ON-14971: Use kernel version as label for sfc module image

--- sfc/mco/sfc-pull-kernel-module-image.sh
+++ sfc/mco/sfc-pull-kernel-module-image.sh
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: (c) Copyright 2023 Advanced Micro Devices, Inc.
 
-if podman image exists image-registry.openshift-image-registry.svc:5000/openshift-kmm/sfc-module; then
+if podman image exists image-registry.openshift-image-registry.svc:5000/openshift-kmm/sfc-module:$(uname -r); then
     echo "Image sfc-module found in the local registry.Nothing to do"
 else
     echo "Image sfc-module not found in the local registry, pulling"
-    podman pull --authfile /var/lib/kubelet/config.json image-registry.openshift-image-registry.svc:5000/openshift-kmm/sfc-module
+    podman pull --authfile /var/lib/kubelet/config.json image-registry.openshift-image-registry.svc:5000/openshift-kmm/sfc-module:$(uname -r)
     if [ $? -eq 0 ]; then
         echo "Image sfc-module has been successfully pulled, rebooting.."
         reboot
