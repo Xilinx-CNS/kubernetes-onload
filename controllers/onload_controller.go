60f4ecbeee0fd7bd70288c2d0a9126d7b70b144f
Ivan Teterevkov <ivan.teterevkov@amd.com>
2023-08-16 14:52:35 +0100

    ON-15121: Create control plane daemon set

--- controllers/onload_controller.go
+++ controllers/onload_controller.go
@@ -7,14 +7,31 @@ import (
 	"context"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	onloadv1alpha1 "github.com/Xilinx-CNS/kubernetes-onload/api/v1alpha1"
+	"github.com/Xilinx-CNS/kubernetes-onload/internal/utils"
 )
+
+const cpdsLabel = "onload.amd.com/name"
+const cpdsSuffix = "-onload-cplane-ds"
+const cpdsPodSuffix = "-onload-cplane"
+
+const cpdsScript = `
+echo /usr/bin/crictl | tee /sys/module/onload/parameters/cplane_server_path;
+declare -r container_id=$(awk -F'[-./]' '/crio-/{print $(NF - 1); exit}' /proc/self/cgroup);
+echo exec ${container_id} /opt/onload/sbin/onload_cp_server -K | tee /sys/module/onload/parameters/cplane_server_params;
+sleep infinity;
+`
 
 // OnloadReconciler reconciles a Onload object
 type OnloadReconciler struct {
@@ -22,9 +39,96 @@ type OnloadReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+func (r *OnloadReconciler) createControlPlaneDaemonSet(
+	ctx context.Context, onload *onloadv1alpha1.Onload,
+) error {
+	dsName := onload.Name + cpdsSuffix
+
+	// Create the control plane daemon set only if it has not been created.
+	ds := &appsv1.DaemonSet{}
+	err := r.Get(
+		ctx,
+		types.NamespacedName{
+			Name:      dsName,
+			Namespace: onload.Namespace,
+		},
+		ds,
+	)
+	if err == nil || !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	container := v1.Container{
+		Name:            onload.Name + cpdsPodSuffix,
+		Image:           onload.Spec.Onload.UserImage,
+		ImagePullPolicy: v1.PullAlways,
+		Command:         []string{"/bin/sh", "-c"},
+		Args:            []string{cpdsScript},
+		SecurityContext: &v1.SecurityContext{
+			Privileged: utils.NewBoolPointer(true),
+		},
+	}
+
+	dsLabels := map[string]string{
+		cpdsLabel: dsName,
+	}
+
+	ds = &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dsName,
+			Namespace: onload.Namespace,
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{MatchLabels: dsLabels},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: dsLabels,
+				},
+				Spec: v1.PodSpec{
+					ServiceAccountName: onload.Spec.ServiceAccountName,
+					NodeSelector:       onload.Spec.Selector,
+					HostNetwork:        true,
+					HostPID:            true,
+					HostIPC:            true,
+					Containers:         []v1.Container{container},
+				},
+			},
+		},
+	}
+
+	err = controllerutil.SetControllerReference(onload, ds, r.Scheme)
+	if err != nil {
+		return err
+	}
+
+	return r.Create(ctx, ds)
+}
+
+func (r *OnloadReconciler) deleteControlPlaneDaemonSet(
+	ctx context.Context, onload *onloadv1alpha1.Onload,
+) error {
+	dsName := onload.Name + cpdsSuffix
+
+	ds := &appsv1.DaemonSet{}
+	err := r.Get(
+		ctx,
+		types.NamespacedName{
+			Name:      dsName,
+			Namespace: onload.Namespace,
+		},
+		ds,
+	)
+	if err != nil {
+		return err
+	}
+
+	return r.Delete(ctx, ds)
+}
+
 //+kubebuilder:rbac:groups=onload.amd.com,resources=onloads,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=onload.amd.com,resources=onloads/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=onload.amd.com,resources=onloads/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -60,8 +164,16 @@ func (r *OnloadReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		log.Info("Adding new Onload kind")
 	} else {
 		log.Info("Deleting new Onload kind")
+
+		err = r.deleteControlPlaneDaemonSet(ctx, onload)
+		return ctrl.Result{}, err
 	}
 
+	err = r.createControlPlaneDaemonSet(ctx, onload)
+	if err != nil {
+		log.Error(err, "Failed to create control plane daemon set")
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
---
+++ internal/utils/utils.go
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: (c) Copyright 2023 Advanced Micro Devices, Inc.
+
+package utils
+
+func NewBoolPointer(v bool) *bool {
+	return &v
+}
