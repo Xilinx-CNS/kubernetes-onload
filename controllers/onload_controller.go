7bef411fe110ba7f6c1eb978e9ac9e15a2ae0add
Ivan Teterevkov <ivan.teterevkov@amd.com>
2023-08-16 14:52:35 +0100

    ON-15121: Create control plane daemon set

--- controllers/onload_controller.go
+++ controllers/onload_controller.go
@@ -12,15 +12,27 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	kmm "github.com/kubernetes-sigs/kernel-module-management/api/v1beta1"
 
 	onloadv1alpha1 "github.com/Xilinx-CNS/kubernetes-onload/api/v1alpha1"
+	"k8s.io/utils/ptr"
 )
+
+const controlPlaneScript = `
+set -e;
+echo /usr/bin/crictl | tee /sys/module/onload/parameters/cplane_server_path;
+declare -r container_id=$(awk -F'[-./]' '/crio-/{print $(NF - 1); exit}' /proc/self/cgroup);
+echo exec ${container_id} /opt/onload/sbin/onload_cp_server -K | tee /sys/module/onload/parameters/cplane_server_params;
+sleep infinity;
+`
 
 // OnloadReconciler reconciles a Onload object
 type OnloadReconciler struct {
@@ -32,6 +44,7 @@ type OnloadReconciler struct {
 //+kubebuilder:rbac:groups=onload.amd.com,resources=onloads/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kmm.sigs.x-k8s.io,resources=modules,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=onload.amd.com,resources=onloads/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -80,6 +93,11 @@ func (r *OnloadReconciler) Reconcile(
 		return res, err
 	}
 
+	err = r.createControlPlaneDaemonSet(ctx, onload)
+	if err != nil {
+		log.Error(err, "Failed to create control plane daemon set")
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -179,6 +197,80 @@ func createModule(
 	}
 
 	return module, nil
+}
+
+func (r *OnloadReconciler) createControlPlaneDaemonSet(
+	ctx context.Context, onload *onloadv1alpha1.Onload,
+) error {
+	log := log.FromContext(ctx)
+
+	dsName := onload.Name + "-onload-cplane-ds"
+
+	// Create the control plane daemon set only if it has not been created.
+	ds := &appsv1.DaemonSet{}
+	err := r.Get(
+		ctx,
+		types.NamespacedName{
+			Name:      dsName,
+			Namespace: onload.Namespace,
+		},
+		ds,
+	)
+
+	if err == nil {
+		log.Info("The control plane daemon set exists")
+		return nil
+	}
+
+	if !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	container := v1.Container{
+		Name:            onload.Name + "-onload-cplane",
+		Image:           onload.Spec.Onload.UserImage,
+		ImagePullPolicy: onload.Spec.Onload.ImagePullPolicy,
+		Command:         []string{"/bin/sh", "-c"},
+		Args:            []string{controlPlaneScript},
+		SecurityContext: &v1.SecurityContext{
+			Privileged: ptr.To(true),
+		},
+	}
+
+	dsLabels := map[string]string{
+		"onload.amd.com/name": dsName,
+	}
+
+	log.Info("Creating control plane daemon set")
+	ds = &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dsName,
+			Namespace: onload.Namespace,
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{MatchLabels: dsLabels},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: dsLabels,
+				},
+				Spec: v1.PodSpec{
+					ServiceAccountName: onload.Spec.ServiceAccountName,
+					NodeSelector:       onload.Spec.Selector,
+					HostNetwork:        true,
+					HostPID:            true,
+					HostIPC:            true,
+					Containers:         []v1.Container{container},
+				},
+			},
+		},
+	}
+
+	err = controllerutil.SetControllerReference(onload, ds, r.Scheme)
+	if err != nil {
+		return err
+	}
+
+	return r.Create(ctx, ds)
 }
 
 // SetupWithManager sets up the controller with the Manager.
