1c3354766bfe0d421c433502f1e3c907414f46d8
Krish De Souza <Krish.De-Souza@amd.com>
2023-12-04 12:29:04 +0000

    ON-15417: Adjusted sfc kernel loading script to handle missing mtd.ko.kz

--- scripts/machineconfig/sfc-replace-kernel.sh
+++ scripts/machineconfig/sfc-replace-kernel.sh
@@ -7,14 +7,14 @@ echo "Looking for the image: $ONLOAD_MODULE_IMAGE"
 if podman image exists "$ONLOAD_MODULE_IMAGE"; then
     echo "Image $ONLOAD_MODULE_IMAGE found locally, removing in-tree kernel module"
 
-    if podman run --privileged --entrypoint modprobe "$ONLOAD_MODULE_IMAGE" -rd /opt sfc sfc_driverlink; then
+    if podman run -v /lib/modules/"$(uname -r)"/kernel:/opt/lib/modules/"$(uname -r)"/kernel --privileged --entrypoint modprobe "$ONLOAD_MODULE_IMAGE" -rd /opt sfc sfc_driverlink; then
             echo "Successfully removed the in-tree kernel module sfc.ko"
     else
             echo "failed to remove in-tree kernel module sfc.ko"
     fi
 
     echo "Running container image to insert the oot kernel module sfc.ko"
-    if podman run --privileged --entrypoint modprobe "$ONLOAD_MODULE_IMAGE" -d /opt sfc; then
+    if podman run -v /lib/modules/"$(uname -r)"/kernel:/opt/lib/modules/"$(uname -r)"/kernel --privileged --entrypoint modprobe "$ONLOAD_MODULE_IMAGE" -d /opt sfc; then
             echo "OOT kernel module sfc.ko is inserted"
     else
             echo "failed to insert OOT kernel module sfc.ko"
