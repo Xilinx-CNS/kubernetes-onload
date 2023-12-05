db81820e1fd43a638a0f97ac971aa230228e4e58
Thomas Crawley <thomas.crawley@amd.com>
2023-12-05 15:14:49 +0000

    Cleanup: Add new imagePullSecret field to CRD

--- controllers/onload_controller_test.go
+++ controllers/onload_controller_test.go
@@ -923,5 +923,26 @@ var _ = Describe("onload controller", func() {
 			),
 		)
 
+		It("should pass through imagepullsecret", func() {
+			devicePlugin := appsv1.DaemonSet{}
+			devicePluginName := types.NamespacedName{
+				Name:      onload.Name + "-onload-device-plugin-ds",
+				Namespace: onload.Namespace,
+			}
+
+			onload.Spec.ImagePullSecret = &corev1.LocalObjectReference{Name: "Steven"}
+			Expect(k8sClient.Create(ctx, onload)).To(BeNil())
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, devicePluginName, &devicePlugin)
+				return err == nil
+			}, timeout, pollingInterval).Should(BeTrue())
+
+			Expect(devicePlugin.Spec.Template.Spec.ImagePullSecrets).Should(
+				ContainElement(MatchFields(IgnoreExtras, Fields{
+					"Name": Equal("Steven"),
+				})),
+			)
+		})
 	})
 })
