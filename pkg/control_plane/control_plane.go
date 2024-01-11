eabdb1794e9adaeb062fa66703be0e2ffe13020e
Thomas Crawley <thomas.crawley@amd.com>
2024-01-11 17:21:05 +0000

    ON-15512: Don't fail with non cri-o container runtime
    Any container runtime that is compatible with the cri should work fine
    since we are using `crictl` which is designed to run against the
    standard.

--- pkg/control_plane/control_plane.go
+++ pkg/control_plane/control_plane.go
@@ -4,10 +4,16 @@ package control_plane
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/golang/glog"
 )
+
+// I couldn't find a definitive list anywhere, so this is a list of the
+// "graduated" container runtimes from cncf.io
+// https://landscape.cncf.io/card-mode?category=container-runtime
+var knownGoodContainerRuntimes = []string{"cri-o", "containerd"}
 
 // Configure the loaded Onload kernel module to launch
 // the Onload control plane process within a container.
@@ -16,12 +22,22 @@ func Configure(
 	containerID string, kpw KernelParametersWriter,
 ) error {
 	// Split the container runtime type from identifier.
-	containerID, found := strings.CutPrefix(containerID, "cri-o://")
-	if !found {
-		return fmt.Errorf("unsupported container runtime in %s", containerID)
+	runtimeAndIdentifier := strings.Split(containerID, "://")
+	if len(runtimeAndIdentifier) != 2 {
+		return fmt.Errorf("unexpected format of containerID %s", containerID)
 	}
 
-	glog.Info("CRI-O container ID is ", containerID)
+	if !slices.Contains(knownGoodContainerRuntimes, runtimeAndIdentifier[0]) {
+		// Warn, but continue
+		glog.Warningf("Unexpected container runtime: %s."+
+			" Running in a compatible container runtime (CRI) is required."+
+			" Execution will continue, but starting the cplane may fail.",
+			runtimeAndIdentifier[0])
+	}
+	containerID = runtimeAndIdentifier[1]
+
+	glog.Info("Container ID found", "runtime",
+		runtimeAndIdentifier[0], "id", containerID)
 
 	// Set the Onload control plane path.
 	crictlPath := "/usr/bin/crictl"
